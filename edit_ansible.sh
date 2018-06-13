ad768ae34ef9a65591fd05f73a19aabf4acaa603
ekaynar <root@gprfc038.sbu.lab.eng.bos.redhat.com>
2018-06-13 13:10:21 -0400

    pbench ansible

--- edit_ansible.sh
+++ edit_ansible.sh
@@ -18,5 +18,35 @@ done
 echo "" >> /etc/ansible/hosts
 
 
+pbench_agent=`cat ansible_inventory | grep "ansible" |awk {'print $2'} | sed 's/.*=//'`
+
+rm -rf pbench-agent-list
+
+echo "[pbench-agent]" >> /etc/ansible/hosts
+for item in ${pbench_agent}
+do
+        echo $item >> /etc/ansible/hosts
+        echo $item >> pbench-agent-list
+done
+
+
+pbench_webserver=`cat ansible_inventory |grep mon| awk {'print $2'} | sed 's/.*=//'`
+echo "[pbench-webserver]" >> /etc/ansible/hosts
+for item in ${pbench_webserver}
+do
+        echo $item >> /etc/ansible/hosts
+        break
+done
+echo "" >> /etc/ansible/hosts
+
+
+rgws=`cat ansible_inventory |grep rgw| awk {'print $2'} | sed 's/.*=//'`
+echo "[rgws]" >> /etc/ansible/hosts
+for item in ${rgws}
+do
+        echo $item >> /etc/ansible/hosts
+done
+echo "" >> /etc/ansible/hosts
+
 
 	#ssh $item 'ip r ' |grep "192." |awk '{print $9}'
