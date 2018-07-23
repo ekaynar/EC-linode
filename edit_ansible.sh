4f5d6db87429dc53800d31ba33479721512c6daf
ekaynar <root@gprfc038.sbu.lab.eng.bos.redhat.com>
2018-07-23 02:54:55 -0400

    bck

--- edit_ansible.sh
+++ edit_ansible.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-clients=`cat ansible_inventory | grep mon | awk {'print $2'} | sed 's/.*=//'`
+rm /etc/ansible/hosts
+rm pbench-agent-list
+
+clients=`cat ~/ceph-linode/ansible_inventory | grep client | awk {'print $2'} | sed 's/.*=//'`
 
 echo "[cosbench-driver]" >> /etc/ansible/hosts
 for item in ${clients}
@@ -18,7 +21,7 @@ done
 echo "" >> /etc/ansible/hosts
 
 
-pbench_agent=`cat ansible_inventory | grep "ansible" |awk {'print $2'} | sed 's/.*=//'`
+pbench_agent=`cat ~/ceph-linode/ansible_inventory | grep ansible |awk {'print $2'} | sed 's/.*=//'`
 
 rm -rf pbench-agent-list
 
@@ -26,11 +29,19 @@ echo "[pbench-agent]" >> /etc/ansible/hosts
 for item in ${pbench_agent}
 do
         echo $item >> /etc/ansible/hosts
-        echo $item >> pbench-agent-list
 done
 
 
-pbench_webserver=`cat ansible_inventory |grep mon| awk {'print $2'} | sed 's/.*=//'`
+pbench_agent=`cat ~/ceph-linode/ansible_inventory | grep osd |awk {'print $2'} | sed 's/.*=//'`
+
+rm -rf pbench-agent-list
+
+echo "[pbench-agent]" >> /etc/ansible/hosts
+for item in ${pbench_agent}
+do
+        echo $item >> pbench-agent-list
+done
+pbench_webserver=`cat ~/ceph-linode/ansible_inventory |grep client | awk {'print $2'} | sed 's/.*=//'`
 echo "[pbench-webserver]" >> /etc/ansible/hosts
 for item in ${pbench_webserver}
 do
@@ -40,7 +51,7 @@ done
 echo "" >> /etc/ansible/hosts
 
 
-rgws=`cat ansible_inventory |grep rgw| awk {'print $2'} | sed 's/.*=//'`
+rgws=`cat ~/ceph-linode/ansible_inventory |grep rgw| awk {'print $2'} | sed 's/.*=//'`
 echo "[rgws]" >> /etc/ansible/hosts
 for item in ${rgws}
 do
@@ -49,4 +60,4 @@ done
 echo "" >> /etc/ansible/hosts
 
 
-	#ssh $item 'ip r ' |grep "192." |awk '{print $9}'
+
